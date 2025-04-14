256bbf009e576fb3ae2bfc77718221ca1e0833d9
aiggerim <162737141+aiggerim@users.noreply.github.com>
2025-04-14 13:57:33 +0500

    Added SupportHandler handler

---
+++ SupportHandler.java
@@ -0,0 +1,12 @@
+abstract class SupportHandler {
+    protected SupportHandler next;
+
+    public SupportHandler setNext(SupportHandler handler) {
+        this.next = handler;
+        return handler;
+    }
+
+    public abstract void handle(String issue);
+}
+
+
